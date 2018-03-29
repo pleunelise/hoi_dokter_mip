5317fd0a5783b96767e14be6c161706162c9ca53
LAPTOP-05CTDOH5\midas <midas.scheffers@coderclass.nl>
2018-03-29 21:08:00 +0200

    make prefab spawn

--- Assets/midas/scripts/makehumans.cs
+++ Assets/midas/scripts/makehumans.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class makehumans : MonoBehaviour {
 
+	public int[] counter;
+
+	public int people;
+
+	public float seconds, minutes;
+
+	public int sec, min;
+
+
+	public GameObject bg;
+
 	public GameObject doctor;
 
 	public GameObject U_gate;
@@ -11,15 +22,30 @@ public class makehumans : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		bg = GameObject.Find ("bg");
+
 		U_gate = GameObject.Find ("upper_gate");
 
-		GameObject doctor = Instantiate(Resources.Load("doctor")) as GameObject; 
 
-		doctor.transform.position = U_gate.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		minutes = (int)(Time.time / 60f);
+		seconds = (int)(Time.time % 60);
+
+		sec = (int)seconds;
+		min = (int)minutes;
+
+		min = min * 60;
+
+		people = (sec + min + 5 % 5);
+
+		for (int i = 1; i <= people; i++) {
+			Instantiate (doctor, U_gate.transform.position, U_gate.transform.rotation);
+		}
+
 	}
 }
