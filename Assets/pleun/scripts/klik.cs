e67f155cc6351790c2d59bed76f8901263231abb
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-04-04 12:41:57 +0200

    First code

---
+++ Assets/pleun/scripts/klik.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class klik : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+			anim.Play("");
+			if(hit != null){
+				Debug.Log("Hit Collider: " + hit.transform.name);
+			}
+			else {
+				Debug.Log("No colliders hit from mouse click");
+			}
+		}
+	}
+
+	//void OnMouseDown(){
+	//	Debug.Log ("Clicked!");
+	//}
+}
