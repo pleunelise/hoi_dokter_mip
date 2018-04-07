d9ed60f8c127740ad7320cf9a8bad9b05fb86a4b
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-04-07 13:57:41 +0200

    First game

--- Assets/pleun/scripts/klik.cs
+++ Assets/pleun/scripts/klik.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class klik : MonoBehaviour {
+	Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
-			anim.Play("");
-			if(hit != null){
-				Debug.Log("Hit Collider: " + hit.transform.name);
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (mouseWorldPos, Vector2.zero);
+			if (hit == null) {
+				return;
 			}
-			else {
-				Debug.Log("No colliders hit from mouse click");
+			if (hit.transform == null) {
+				return;
+			}
+
+			Debug.Log ("Hit Collider: " + hit.transform.name);
+
+			switch (hit.transform.name) {
+			case "Bureaustoel":
+				animator.Play ("gevstoel");
+				break;
+			case "Prikbord-2":
+				animator.Play ("gevPrikbord");
+				break;	
+			case "Plant":
+				animator.Play ("gevPlent");
+				break;
+			case "Ladekast":
+				animator.Play ("gevKast");
+				break;
+			case "Receptiebordje":
+				animator.Play ("gevBordje");
+				break;
+			case "Computer":
+				animator.Play ("gevondenAnimatie");
+				break;
 			}
 		}
 	}
-
-	//void OnMouseDown(){
-	//	Debug.Log ("Clicked!");
-	//}
 }
+
